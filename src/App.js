a21ddee32768cfd4b19806c3b278e65397c19651
Ahmed Sarhan <ahmedsarhan1515@gmail.com>
2021-06-05 14:08:39 +0200

    final result

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
+import React, { useState, useEffect } from 'react';
 import './App.css';
-
 import { ReactComponent as ClockIcon } from './icons/clock.svg';
 import { ReactComponent as CalenderIcon } from './icons/calendar.svg';
 
 function App() {
+	const [dateState, setDateState] = useState(new Date());
+	useEffect(() => {
+		setInterval(() => setDateState(new Date()), 3000);
+	}, []);
 	return (
 		<div className="App">
 			<CalenderIcon />
-			<p>date</p>
+			<p>
+				{' '}
+				{dateState.toLocaleDateString('en-GB', {
+					day: 'numeric',
+					month: 'short',
+					year: 'numeric',
+				})}
+			</p>
 			<ClockIcon />
-			<p>time</p>
+			<p>
+				{dateState.toLocaleString('en-US', {
+					hour: 'numeric',
+					minute: 'numeric',
+					hour12: true,
+				})}
+			</p>
 		</div>
 	);
 }
